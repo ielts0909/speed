1b00d40f4d87728aac28f134c07d17d2d29e2623
gais.ch <gais.ch@alibaba-inc.com>
2015-05-02 06:21:48 +0800

    netty test

---
+++ network/src/test/java/test/netty/discard/DiscardServerHandler.java
@@ -0,0 +1,26 @@
+package test.netty.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * User: gais.ch
+ * Date: 15-5-1
+ * Time: 下午12:08
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
